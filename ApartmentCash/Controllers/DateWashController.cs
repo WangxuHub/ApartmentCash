e5c2ceda6e0b0c996b9be4f9fde4d4b125402f4d
chenshuangyu <695166116@qq.com>
2016-09-27 22:55:41 +0800

    排洗 系统

--- ApartmentCash/Controllers/DateWashController.cs
+++ ApartmentCash/Controllers/DateWashController.cs
@@ -8,32 +8,50 @@ using System.Web.Mvc;
 namespace ApartmentCash.Controllers
 {
     [Authorize(Users ="管理员")]
+    //[Route("DateWash/{action}")]
     public class DateWashController : Controller
     {
         [AllowAnonymous]
+        [Route("DateWash/{year:int}/{month:int}")]
+        [Route("DateWash/{year:int}")]
+        [Route("DateWash")]
         // GET: DateWash
-        public ActionResult Index(int id=1)
+        public ActionResult Index(int year=0,int month=0)
         {
-            int start = id * 10;
-            int end = (id - 1) * 10;
+            if(year<=0)
+            {
+                year = DateTime.Now.Year;
+            }
+            if(month <= 0)
+            {
+                month = DateTime.Now.Month;
+            }
+
+            int start = month * 10;
+            int end = (month - 1) * 10;
             DBModel.ApartmentCashEntities ef = new DBModel.ApartmentCashEntities();
 
+
+            string dateStr = new DateTime(year, month, 1).ToString("yyyy-MM");
             List<Models.DateWashViewModel> list = (
                                                from a in ef.DateWash
                                                join b in ef.AspNetUsers.DefaultIfEmpty() on a.UserID equals b.Id into temp
+                                               where a.DateStr.Contains(dateStr)
                                                orderby a.DateStr
                                                from t in temp.DefaultIfEmpty()
                                                select new Models.DateWashViewModel()
                                                {
                                                    DateStr = a.DateStr,
                                                    UserID = a.UserID,
-                                                   IsFinish= a.IsFinish,
+                                                   IsFinish = a.IsFinish,
                                                    UserName = t.UserName
-                                               }).OrderBy(item=>item.DateStr).Take(start).Skip(end).ToList();
+                                               }).OrderBy(item => item.DateStr).ToList();//.Take(start).Skip(end).ToList();
 
 
             List<DBModel.AspNetUsers> userList = ef.AspNetUsers.Where(item=>item.UserName!="管理员").OrderBy(item => item.UserName).ToList();
             ViewBag.userList = userList;
+            ViewBag.month =ViewBag.pageIndex = month;
+            ViewBag.year = year;
             return View(list);
         }
 
@@ -96,6 +114,7 @@ namespace ApartmentCash.Controllers
             return RedirectToAction("index");
         }
 
+        //按人员排洗
         [HttpPost]
         public ActionResult CreateDateWashPerson()
         {
@@ -147,5 +166,62 @@ namespace ApartmentCash.Controllers
             json.msg = "生成成功";
             return Content(json.ToString());
         }
+
+        //轮空处理
+        [HttpPost]
+        public ActionResult NoInTrun()
+        {
+            string dateStr = Request["dateStr"];
+
+            DBModel.ApartmentCashEntities ef = new DBModel.ApartmentCashEntities();
+
+            System.Data.Entity.Database db = ef.Database;
+
+            using (System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(db.Connection.ConnectionString))
+            {
+                conn.Open();
+                System.Data.SqlClient.SqlTransaction trans = conn.BeginTransaction();
+             
+                try
+                {
+
+
+                    string sql2 = string.Format(@"
+                        update  a
+                        set a.UserID=b.UserID
+                        from DateWash a
+                        inner join DateWash b on a.DateWashID=b.DateWashID+1
+                        where a.DateStr>'{0}'", dateStr);
+
+                    System.Data.SqlClient.SqlCommand cmd2 = new System.Data.SqlClient.SqlCommand(sql2, conn, trans);
+                    cmd2.ExecuteNonQuery();
+
+                    string sql = string.Format("update DateWash set UserID=null where DateStr='{0}'",dateStr);
+
+                    System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand(sql, conn, trans);
+                    cmd.ExecuteNonQuery();
+
+
+                    trans.Commit();
+
+                }
+                catch (Exception ee)
+                {
+                    trans.Rollback();
+                    json.success = false;
+                    json.msg = ee.Message;
+                    if (ee.InnerException != null)
+                    {
+                        json.msg += "  " + ee.InnerException.Message;
+                    }
+                }
+            }
+
+            
+            json.success = true;
+            json.msg = "操作成功";
+            return Content(json.ToString());
+        }
+
     }
 }
